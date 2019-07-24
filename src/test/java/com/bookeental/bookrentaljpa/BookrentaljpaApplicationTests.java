ab71d3eaa36b626db12c35855da018799330a7a4
battulajhansi <jhansibtech123@gmail.com>
2019-07-24 11:22:36 +0530

    BookRentalJpaBackend

---
+++ src/test/java/com/bookeental/bookrentaljpa/BookrentaljpaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bookeental.bookrentaljpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookrentaljpaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
